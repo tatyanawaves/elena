ccb53979e06ddf43a4cc79438c5078af0fb02d2a
tatyanawaves <tatyanawaves@gmail.com>
2026-04-09 09:39:38 +0500

    feat: Elena Kolos Academy - full course platform with rich previews
    Convex + React + Vite + shadcn/ui + Tailwind CSS
    - 8 courses (trainings, games, YouTube videos)
    - Rich course pages: hero, outcomes, learning process, FAQ, author bio
    - Accurate author credentials (Dr. Econ., Academician MAI, MBA)
    - User enrollment & authentication via Convex Auth
    - Stripe integration ready

---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+export { default as LandingPage } from "./LandingPage";
+export { default as CoursePage } from "./CoursePage";
+export { default as MyCoursesPage } from "./MyCoursesPage";
+export { LoginPage } from "./LoginPage";
+export { SignupPage } from "./SignupPage";
+export { SettingsPage } from "./SettingsPage";
+export { DashboardPage } from "./DashboardPage";
